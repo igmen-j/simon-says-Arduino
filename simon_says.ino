7c72ce1bc5fd46fda369d165f2c0c95d2b77a71f
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2020-07-06 21:37:49 -0600

    Add files via upload

---
+++ simon_says.ino
@@ -0,0 +1,46 @@
+#define NUM_COMPONENTS 4
+#define MAX_LEVEL 10
+
+const int button[NUM_COMPONENTS] = {2,3,4,5};
+const int led[NUM_COMPONENTS] = {6,7,8,9};
+
+int currentLevel = 0;
+int sequence[MAX_LEVEL];
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < NUM_COMPONENTS; i++){
+    pinMode(button[i], INPUT);
+    pinMode(led[i], OUTPUT);
+    digitalWrite(led[i], LOW);
+  }
+}
+
+void loop() {
+
+  startSequence();
+  delay(2000);  //2 second delay
+  getSequence(currentLevel, sequence);
+  showSequence(currentLevel, sequence);
+  
+  for (int i = 0; i < NUM_COMPONENTS; i++)
+  {
+    if (digitalRead(button[i]))
+      digitalWrite(led[i], HIGH);
+    else
+      digitalWrite(led[i], LOW);
+  }
+
+}
+
+void startSequence(){
+  // start sequences goes here
+  // LED and sounds 
+}
+
+void getSequence(int currentLevel, int sequence[]) {
+  int randNum = random(0,4);
+  sequence[currentLevel] = randNum;  
+}
+
+void showSequence(int);
