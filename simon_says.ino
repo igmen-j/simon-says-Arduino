3317334002db8ca6ce57cfd605b21c4a4b2437f7
Justin Igmen <55956362+igmen-j@users.noreply.github.com>
2020-07-12 21:15:51 -0600

    Add files via upload
    - Very buggy but completed main part of the program
    - Program now generates sequence at the beginning
    - Takes user input and compares it to the sequence
    - Added lives
    
    **Code almost done-- just need testing

--- simon_says.ino
+++ simon_says.ino
@@ -1,10 +1,9 @@
 #define NUM_COMPONENTS 4
-#define MAX_LEVEL 10
+#define MAX_LEVEL 16
 
 const int button[NUM_COMPONENTS] = {2,3,4,5};
 const int led[NUM_COMPONENTS] = {6,7,8,9};
 
-int currentLevel = 0;
 int sequence[MAX_LEVEL];
 
 void setup() {
@@ -18,20 +17,19 @@ void setup() {
 }
 
 void loop() {
+  bool endCondition;
+  
   startSequence();
   //delay(2000);  //2 second delay
-  getSequence(currentLevel, sequence);
-  showSequence(currentLevel, sequence);
-  /*
-  for (int i = 0; i < NUM_COMPONENTS; i++)
-  {
-    if (digitalRead(button[i]))
-      digitalWrite(led[i], HIGH);
-    else
-      digitalWrite(led[i], LOW);
-  }
-  */
+  getSequence();
+  endCondition = startGame();
 
+  if (endCondition == true)
+    loseSequence();
+  else
+    winSequence();
+    
+  delay(100000000);
 }
 
 void startSequence(){
@@ -39,19 +37,122 @@ void startSequence(){
   // LED and sounds 
 }
 
-void getSequence(int currentLevel, int sequence[]) {
-  long randNum = random(100);
-  randNum %= 4;
-  Serial.println(randNum);
-  sequence[currentLevel] = randNum;  
+/*
+ * Generates a random numbers and stores it in an array -- not truly random
+ */
+void getSequence() {
+  int randNum;
+  Serial.println("sequence:");
+  for (int i = 0; i < MAX_LEVEL; i++) {
+    randNum = random(4);
+    
+    while ((randNum == sequence[i-1]) && (sequence[i-1] == sequence[i-2])){   //Loop so that value cannot appear more than 2 times
+      randNum = random(4);
+    }
+     
+    sequence[i] = randNum;  
+    Serial.println(sequence[i]);
+  }
+  Serial.println("done");
+  
+  return;
 }
 
-void showSequence(int currentLevel, int sequence[]) {
-  digitalWrite(led[sequence[currentLevel]], HIGH);
-  delay(1000); 
-     for (int i = 0; i < NUM_COMPONENTS; i++){
+bool startGame(){
+  int userVal, currentVal = 0, currentLevel = 0, lives = 3;
+  bool isGameOver = false, endCondition;
 
-    digitalWrite(led[i], LOW);
+  while ((isGameOver == false) || (currentLevel == MAX_LEVEL))
+  {
+    showSequence(currentLevel);
+
+    Serial.print("Level: ");
+    Serial.println(currentLevel+1);
+    Serial.print("Lives: ");
+    Serial.println(lives);
+  
+    while (currentVal != currentLevel+1){
+      userVal = getUserInput();
+      if (compareValue(currentVal, userVal) == true){
+        currentVal++;
+      }
+      else{
+        lives--;
+        if (lives != 0)
+          currentVal = 0;
+        else{
+          isGameOver = true;
+          endCondition = true;
+          break;
+        }
+      }
+    }
+    
+    if (isGameOver == false){
+      currentLevel++;
+      if (currentLevel == endCondition)
+        endCondition == false;
+    }
   }
-    delay(1000);
+
+  return endCondition;
+}
+
+
+
+
+/*
+ * Displays the generated sequence
+ */
+void showSequence(int currentLevel) {
+  for (int i = 0; i < currentLevel+1; i++){
+    digitalWrite(led[sequence[i]], HIGH);
+    delay(1000); 
+    digitalWrite(led[sequence[i]], LOW);
+  }
+
+  return;
+}
+
+//need to put timer here too!
+int getUserInput() {
+  bool buttonPressed = false;
+  int userVal;
+  while (!buttonPressed){
+    for (int i = 0; i < NUM_COMPONENTS; i++) {
+      if (digitalRead(button[i])) {
+        buttonPressed = true;
+        userVal = i;
+        break;
+      }
+    }
+  }
+  return userVal;
+}
+
+/*
+ * Compares if input value is the same, return true -- current state: turn on all lights if correct
+ * If not, return false
+ */
+bool compareValue(int currentVal, int userVal){
+  if (sequence[currentVal] == userVal) {
+    for (int i = 0; i < NUM_COMPONENTS; i++){
+      digitalWrite(led[i], HIGH);
+    }
+    delay(1000); 
+    for (int i = 0; i < NUM_COMPONENTS; i++){
+    digitalWrite(led[i], LOW);
+    }
+    return true;
+  }
+  else
+    return false;
+}
+
+void loseSequence(){
+  
+}
+
+void winSequence(){
+  
 }
